9cf50d16727728ea815b0ea8af06d9d0839bb9aa
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-11-19 10:26:47 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:crud_app/providers/client_provider.dart';
+import 'package:crud_app/views/clients_list.dart';
+import 'package:crud_app/views/formulaire.dart';
+import 'package:crud_app/routes/routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (ctx) => ClientProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.grey),
+          useMaterial3: true,
+        ),
+        routes: {
+          Routes.INICIAL: (_) => const ClientsList(),
+          Routes.FORMULARIO: (_) => Formulaire(), // Certifique-se que o construtor pode ser `const`
+        },
+      ),
+    );
+  }
+}
